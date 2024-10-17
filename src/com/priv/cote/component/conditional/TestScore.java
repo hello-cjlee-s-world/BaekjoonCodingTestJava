41e5b1a0dfb54e93096c64c34e89f56e46bf38fa
hello-cjlee-s-world <153203021+hello-cjlee-s-world@users.noreply.github.com>
2024-10-17 22:36:07 +0900

    2단계 조건문 시험 성적

---
+++ src/com/priv/cote/component/conditional/TestScore.java
@@ -0,0 +1,18 @@
+package com.priv.cote.component.conditional;
+
+import java.io.*;
+
+public class TestScore {
+    public void score () throws IOException {
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        int score = Integer.parseInt(br.readLine());
+        if(score >= 90) bw.append("A");
+        else if (score >= 80) bw.append("B");
+        else if (score >= 70) bw.append("C");
+        else if (score >= 60) bw.append("D");
+        else bw.append("F");
+        bw.flush();
+        bw.close();
+    }
+}
