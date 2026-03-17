f407ed857155c29501575b04ad8b00317c99ef36
hello-cjlee-s-world <cndwn2131@naver.com>
2026-03-17 17:37:32 +0900

    11단계 시간 복잡도 알고리즘 수업 - 알고리즘의 수행 시간 3

---
+++ src/com/priv/cote/component/timeComplexity/Algorythm3.java
@@ -0,0 +1,19 @@
+package com.priv.cote.component.timeComplexity;
+
+import com.priv.cote.main.BasicInterface;
+
+import java.io.*;
+
+public class Algorythm3 implements BasicInterface {
+    @Override
+    public void print() throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+      long a = Integer.parseInt(br.readLine());
+
+      bw.write(a*a+"\n" + "2");
+
+      bw.flush();
+      bw.close();
+    }
+}
--- src/com/priv/cote/main/Main.java
+++ src/com/priv/cote/main/Main.java
@@ -1,9 +1,9 @@
 package com.priv.cote.main;
 
-import com.priv.cote.component.timeComplexity.Algorythm2;
+import com.priv.cote.component.timeComplexity.Algorythm3;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-      new Algorythm2().print();
+      new Algorythm3().print();
     }
 }
