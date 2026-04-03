7f56e2834bb402535dcf41da43436ad50c996c77
hello-cjlee-s-world <cndwn2131@naver.com>
2026-04-03 11:18:29 +0900

    13단계 정렬 좌표 정렬하기 2

--- src/com/priv/cote/main/Main.java
+++ src/com/priv/cote/main/Main.java
@@ -1,9 +1,9 @@
 package com.priv.cote.main;
 
-import com.priv.cote.component.sort.CoordinateSort;
+import com.priv.cote.component.sort.CoordinateSort2;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-      new CoordinateSort().print();
+      new CoordinateSort2().print();
     }
 }
